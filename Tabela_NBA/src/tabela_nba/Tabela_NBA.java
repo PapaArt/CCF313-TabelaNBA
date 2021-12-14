e84ccfba723b6d44c2c6a6c1a8c5d92905aa0fc6
Vinícius <vinicius.o.mendes@ufv.br>
2021-12-14 20:23:16 -0300

    modif time.java + tabela.java

--- Tabela_NBA/src/tabela_nba/Tabela_NBA.java
+++ Tabela_NBA/src/tabela_nba/Tabela_NBA.java
@@ -8,24 +8,16 @@ import java.util.Scanner;
  * @author vinicius
  */
 public class Tabela_NBA {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String nomesTimes[] = {"Golden State Warriors","Boston Celtics","Brooklyn Nets","Los Angeles Lakers"};
+    
+    Scanner scan = new Scanner(System.in);  
+    private Time time1;
+    
+    private int quantidadeJogos;
+    
         
-        System.out.println("Indique o confronto:");
-        System.out.println("1 para: "+nomesTimes[0]);
-        System.out.println("2 para: "+nomesTimes[1]);
-        System.out.println("3 para: "+nomesTimes[2]);
-        System.out.println("4 para: "+nomesTimes[3]);
-        
-      
-        System.out.print("Time 1: ");
-        int time1 = scan.nextInt();
-        System.out.print("Time 2: ");
-        int time2 = scan.nextInt();
-        
-        System.out.println("O confronto eh: "+nomesTimes[time1-1]+" vs "+nomesTimes[time2-1]);
-        
-        
+    public Tabela_NBA(){
+        time1 = new Time();
+        this.quantidadeJogos = 0;
     }
+
 }
