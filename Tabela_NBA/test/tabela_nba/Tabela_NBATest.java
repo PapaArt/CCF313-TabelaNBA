aac9199fb178761793225878fa0a9bfdc711c0c8
Vinícius <vinicius.o.mendes@ufv.br>
2021-12-14 20:24:57 -0300

    testbench

--- Tabela_NBA/test/tabela_nba/Tabela_NBATest.java
+++ Tabela_NBA/test/tabela_nba/Tabela_NBATest.java
@@ -43,7 +43,7 @@ public class Tabela_NBATest {
     public void testMain() {
         System.out.println("main");
         String[] args = null;
-        Tabela_NBA.main(args);
+        //Tabela_NBA.main(args);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
