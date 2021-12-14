303137a1bc0cdcc29864d187f6703d205d67122a
Vinícius <vinicius.o.mendes@ufv.br>
2021-12-14 13:28:23 -0300

    add time.java + modif

--- Tabela_NBA/src/tabela_nba/Time.java
+++ Tabela_NBA/src/tabela_nba/Time.java
@@ -7,4 +7,13 @@ package tabela_nba;
  */
 public class Time {
     private String nomeTime;
+    
+    //conferência: 0 para Leste 1 para Oeste
+    private int conferencia = 0;
+        
+    private int vitorias = 0;
+    private int derrotas = 0;
+    private int jogosAtras = 0; // 0 = sem jogos atrás
+        
+    private double porcentagemVitoria = 0;
 }
