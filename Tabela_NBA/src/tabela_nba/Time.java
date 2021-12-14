9d2f4eab19baf89a575fcab55496d50485b42c80
Vinícius <vinicius.o.mendes@ufv.br>
2021-12-14 13:24:56 -0300

    add time.java

--- Tabela_NBA/src/tabela_nba/Time.java
+++ Tabela_NBA/src/tabela_nba/Time.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 package tabela_nba;
 
@@ -10,5 +6,5 @@ package tabela_nba;
  * @author vinicius
  */
 public class Time {
-
+    private String nomeTime;
 }
