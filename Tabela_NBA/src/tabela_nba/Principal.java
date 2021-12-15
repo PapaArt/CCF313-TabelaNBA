2ccbc8ceccf0b5ee5935a5e5cce51ef5830b8498
PapaArt <artur.papa@ufv.br>
2021-12-14 22:17:33 -0300

    Acessando as variaveis

--- Tabela_NBA/src/tabela_nba/Principal.java
+++ Tabela_NBA/src/tabela_nba/Principal.java
@@ -7,6 +7,14 @@ package tabela_nba;
  */
 public class Principal {
     public static void main(String[] args) {
-        Time time = new Time();
+        Tabela_NBA tabela = new Tabela_NBA();
+        Time team = new Time();
+        
+        int defineQuantidadeJogos = tabela.defineQuantidadeJogos();
+//        tabela.time1.ganhou();
+//        tabela.time1.aproveitamento();
+        int vitorias = team.ganhou();
+        System.out.println("Vitorias: "+vitorias);
+        System.out.println("Jogos: "+defineQuantidadeJogos);
     }
 }
