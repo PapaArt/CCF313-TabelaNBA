7c93d6b23e49240f1d8a30783953c4bb82a9c09d
Vinícius <vinicius.o.mendes@ufv.br>
2021-12-14 21:05:38 -0300

    update tabela

--- Tabela_NBA/src/tabela_nba/Tabela_NBA.java
+++ Tabela_NBA/src/tabela_nba/Tabela_NBA.java
@@ -13,11 +13,40 @@ public class Tabela_NBA {
     private Time time1;
     
     private int quantidadeJogos;
+    //conferência: 1 para Leste / 0 para Oeste
+    private int conferencia;
     
+    private String[] nomesTimes = new String[15];
         
     public Tabela_NBA(){
         time1 = new Time();
         this.quantidadeJogos = 0;
+        this.conferencia = 0;
+        for (int i = 0; i < 15; i++) {
+            this.nomesTimes[i] = "";
+        }
+    }
+    
+    public int defineConferencia(int conferenciaTabela){
+        switch (conferenciaTabela) {
+            case 0:
+                this.conferencia = conferenciaTabela;
+                System.out.println("Conferencia Oeste");
+                return this.conferencia;
+            case 1:
+                this.conferencia = conferenciaTabela;
+                System.out.println("Conferencia Leste");
+                return this.conferencia;
+            default:
+                System.err.println("Conferência invalida!!!");
+                return 0;
+        }
+    }
+    
+    public int defineQuantidadeJogos(){
+        this.quantidadeJogos = time1.numeroJogos();
+        
+        return quantidadeJogos;
     }
 
 }
