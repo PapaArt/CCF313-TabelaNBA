60dcf3a5e3930410b409c626be1326918631fe66
PapaArt <artur.papa@ufv.br>
2021-12-19 16:11:03 -0300

    Vetor de times

--- Tabela_NBA/src/tabela_nba/Principal.java
+++ Tabela_NBA/src/tabela_nba/Principal.java
@@ -14,21 +14,21 @@ public class Principal {
         //int defineQuantidadeJogos = tabela.defineQuantidadeJogos();
 //        tabela.time1.ganhou();
 //        tabela.time1.aproveitamento();
-        tabela.time1.setVitorias(6);
-        tabela.time1.setDerrotas(6);
-        tabela.time2.setVitorias(7);
-        tabela.time2.setDerrotas(3);
+        tabela.times[0].setVitorias(6);
+        tabela.times[0].setDerrotas(6);
+        tabela.times[1].setVitorias(7);
+        tabela.times[1].setDerrotas(3);
         
-        System.out.println("Nome time 1: "+tabela.time1.getNomeTime());
-        System.out.println("Nome time 2: "+tabela.time2.getNomeTime());
+        System.out.println("Nome time 1: "+tabela.times[0].getNomeTime());
+        System.out.println("Nome time 2: "+tabela.times[1].getNomeTime());
         System.out.println();
-        System.out.println("get vitorias time 1: " +tabela.time1.getVitorias());
-        System.out.println("get jogos time 1: "+tabela.time1.getJogos());
-        System.out.println("Porcentagem de vitoria time 1: " + (tabela.time1.getPorcentagemVitoria())* 100 + "%");
+        System.out.println("get vitorias time 1: " +tabela.times[0].getVitorias());
+        System.out.println("get jogos time 1: "+tabela.times[0].getJogos());
+        System.out.println("Porcentagem de vitoria time 1: " + (tabela.times[0].getPorcentagemVitoria())* 100 + "%");
         System.out.println();
-        System.out.println("get vitorias time 1: " +tabela.time2.getVitorias());
-        System.out.println("get jogos time 1: "+tabela.time2.getJogos());
-        System.out.println("Porcentagem de vitoria time 1: " + (tabela.time2.getPorcentagemVitoria())* 100 + "%");
+        System.out.println("get vitorias time 1: " +tabela.times[1].getVitorias());
+        System.out.println("get jogos time 1: "+tabela.times[1].getJogos());
+        System.out.println("Porcentagem de vitoria time 1: " + (tabela.times[1].getPorcentagemVitoria())* 100 + "%");
         
     }
 }
